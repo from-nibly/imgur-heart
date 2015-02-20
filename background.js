d39cf18e50fb8047e5e5681580fa71535db765b4
Eric Volpert <ericvolp12@gmail.com>
2015-02-20 17:06:34 +0000

    Added Return-of-The-Bar to Imgur-Heart

--- background.js
+++ background.js
@@ -16,10 +16,11 @@ $(document).click(function() {
 });
 
 $(document).ready(function() {
+	$(".stats-link").after('<div class="progress" style="display:inline-block; width:20%; height:20%; vertical-align:middle;"><div class="progress-bar progress-bar-success" role="progressbar"></div><div class="progress-bar progress-bar-danger" role="progressbar"></div></div>');
   var button = $(".favorite-image");
 
-  var points = $(".stats-link.left");
-  
+  var points = $(".progress");
+
   points.after('<div class="tag-holder"></div>');
 	generateTags();
 });
@@ -30,6 +31,42 @@ function generateTags() {
   holder.empty();
   tags.each(function(index) {
     var tagName = $(this).text();
-    holder.append('<a class="tag-link" href="/t/' + tagName + '">' + tagName + '</a>' + (tags.length === index + 1 ? '' : '<br>'));
+    holder.append('<br><a class="tag-link" href="/t/' + tagName + '">' + tagName + '</a>' + (tags.length === index + 1 ? '' : ''));
   });
+}
+var ups;
+var downs;
+
+window.onload = function(){
+
+window.setTimeout(function(){
+  var imageID = window.location.href.replace("http://imgur.com/gallery/", "");
+
+  $.ajax
+({
+  type: "GET",
+  url: "https://api.imgur.com/3/gallery/image/"+imageID+"/votes",
+  dataType: 'json',
+  async: true,
+  headers: {
+    "Authorization": "Client-ID " + "d25fe2851fe0e96"
+  },
+  success: function (result){
+
+    ups = result.data.ups;
+    downs = result.data.downs;
+
+    percentUp = ((ups)/(ups+downs))*100;
+
+
+    percentDown = ((downs)/(ups+downs))*100;
+
+
+
+    $(".progress-bar-success").css("width", percentUp+"%");
+    $(".progress-bar-danger").css("width", percentDown+"%");
+
+  }
+});
+}, 50);
 }
